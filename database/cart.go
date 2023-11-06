6127e4560344a8278df323deff0af1e34c76eccc
unknown <bhavesh.khandelwal19@gmail.com>
2023-11-07 01:33:54 +0530

    Implementing database connetion to mongoDB

---
+++ database/cart.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"errors"
+)
+
+var (
+	ErrCantFindProduct    = errors.New("cant find the product")
+	ErrCantDecodeProducts = errors.New("cant fint he product")
+	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrCantRemoveItemCart = errors.New("cannot add this product to cart")
+	ErrCantUpdateUser     = errors.New("cannot remove this item from the cart")
+	ErrCantGetItem        = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() {}
+
+func RemoveCartItem() {}
+
+func BuytItemFromCart() {}
+
+func InstantBuyer() {}
